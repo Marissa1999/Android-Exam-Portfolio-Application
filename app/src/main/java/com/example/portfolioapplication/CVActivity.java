1d27c186de208d2d359d3bcb7207c721ce6d4751
Marissa1999 <marissagoncalves13@gmail.com>
2019-12-02 14:49:41 -0500

    Enabled the navigation with fragments.

--- app/src/main/java/com/example/portfolioapplication/CVActivity.java
+++ app/src/main/java/com/example/portfolioapplication/CVActivity.java
@@ -2,13 +2,19 @@ package com.example.portfolioapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class CVActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+public class CVActivity extends AppCompatActivity
+{
+    private final Fragment profileFragment = new ProfileFragment();
+    private final Fragment educationFragment = new EducationFragment();
+    private final Fragment experienceFragment = new ExperienceFragment();
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
+    Fragment active = profileFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,13 +23,16 @@ public class CVActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_home);
+                    fragmentManager.beginTransaction().hide(active).show(profileFragment).commit();
+                    active = profileFragment;
                     return true;
                 case R.id.navigation_education:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragmentManager.beginTransaction().hide(active).show(educationFragment).commit();
+                    active = profileFragment;
                     return true;
                 case R.id.navigation_experience:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragmentManager.beginTransaction().hide(active).show(experienceFragment).commit();
+                    active = profileFragment;
                     return true;
             }
             return false;
@@ -36,8 +45,12 @@ public class CVActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cv);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fragmentManager.beginTransaction().add(R.id.cv_container, profileFragment, "1").commit();
+        fragmentManager.beginTransaction().add(R.id.cv_container, educationFragment, "2").hide(educationFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.cv_container, experienceFragment, "3").hide(experienceFragment).commit();
+
+
     }
 
 }
