bbfb66a6ca4863aee59ff2c87219e16d8ee705ef
Marissa1999 <marissagoncalves13@gmail.com>
2019-12-02 15:00:15 -0500

    Create the MainActivity layout.

--- app/src/main/java/com/example/portfolioapplication/MainActivity.java
+++ app/src/main/java/com/example/portfolioapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.portfolioapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchCVActivity(View view)
+    {
+        Intent cvIntent = new Intent(this, CVActivity.class);
+        startActivity(cvIntent);
+
+        String launchingMainMessage = "Feel Free to View My Profile";
+        Toast toastMainMessage = Toast.makeText(this, launchingMainMessage, Toast.LENGTH_SHORT);
+        toastMainMessage.show();
     }
 }
